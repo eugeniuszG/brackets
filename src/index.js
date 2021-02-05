7efd3c52e3efbd769357d973ff100d388a00a6a2
Eugeniusz Gronski <41344500+eugeniuszG@users.noreply.github.com>
2021-02-05 03:41:47 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strArr = str.split('');
+  let infLoop = true;
+
+  while (infLoop) {
+    //prevent inf loop if do not exec 17line
+    infLoop = false;
+
+    for (let i = 0, leni = strArr.length; i < leni; i++) {
+      for (let j = 0, lenj = bracketsConfig.length; j < lenj; j++) {
+
+        let openBracket = bracketsConfig[j][0];
+        let closeBracket = bracketsConfig[j][1];
+
+        if ((strArr[i] === openBracket) && (strArr[i + 1] === closeBracket)) {
+          //delete valid brackets from incoming str
+          strArr.splice(i, 2);
+          (strArr.length === 0) ? infLoop = false: infLoop = true;
+        }
+      }
+    }
+  }
+  return strArr.length === 0;
 }
